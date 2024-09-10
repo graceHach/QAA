f95920b060f2d2b6be70b0aa4e77abc78f6888e4
Grace Hach <ghach@n0349.talapas.uoregon.edu>
2024-09-10 12:03:31 -0700

    Updated notes/environment documentation with info about error fix

--- plot_length_dist.sh
+++ plot_length_dist.sh
@@ -2,8 +2,8 @@
 
 python plot_length_dist.py -color1 "darkgreen" -color2 "limegreen" -color3 "k" -color4 "darkgray" \
  -title "17 3E Fox Trimmed Read Lengths Distribution" -f_out "17_trimmed_lengths" \
- -r1_paired_in "15_R1_adapters_removed_paired.fq.gz"  -r1_unpaired_in "15_R1_adapters_removed_unpaired.fq.gz" \
- -r2_paired_in "15_R2_adapters_removed_paired.fq.gz"  -r2_unpaired_in "15_R2_adapters_removed_unpaired.fq.gz" 
+ -r1_paired_in "17_R1_adapters_removed_paired.fq.gz"  -r1_unpaired_in "17_R1_adapters_removed_unpaired.fq.gz" \
+ -r2_paired_in "17_R2_adapters_removed_paired.fq.gz"  -r2_unpaired_in "17_R2_adapters_removed_unpaired.fq.gz" 
 
 # Running with defaults gives plot for lib 15
 python plot_length_dist.py -color1 "blue" -color2 "cornflowerblue" -color3 "k" -color4 "darkgray"
